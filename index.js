1b5cd55bbc3cc4a8b864109ed7935eebba5dc999
Halyn-gif <dolinagalina9@gmail.com>
2025-10-01 15:39:17 +0200

    new package.json script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello Collin')
 })
 
 console.log("Server is running on port 3000....");
